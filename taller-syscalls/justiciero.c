e966333017855d865b2acc98d055fc89bf1393c5
NicoAnsaldi <nansaldi611@gmail.com>
2018-03-26 09:25:11 -0300

    Listo el ej2

--- taller-syscalls/justiciero.c
+++ taller-syscalls/justiciero.c
@@ -1,8 +1,8 @@
 #include <sys/ptrace.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/reg.h>
 #include <unistd.h>
+#include <syscall.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -21,6 +21,9 @@ int main(int argc, char* argv[]) {
 	if (child == -1) { perror("ERROR fork"); return 1; }
 	if (child == 0) {
 		/* S'olo se ejecuta en el Hijo */
+		if (ptrace(PTRACE_TRACEME, 0, NULL, NULL)) {
+			perror("ERROR child ptrace(PTRACE TRACEME, ...)"); exit(1);
+		}
 		execvp(argv[1], argv+1);
 		/* Si vuelve de exec() hubo un error */
 		perror("ERROR child exec(...)"); exit(1);
@@ -29,7 +32,18 @@ int main(int argc, char* argv[]) {
 		while(1) {
 			if (wait(&status) < 0) { perror("waitpid"); break; }
 			if (WIFEXITED(status)) break; /* Proceso terminado */
+			if(ptrace(PTRACE_PEEKUSER, child, 8 * RAX, NULL) < 0){
+				if(ptrace(PTRACE_PEEKUSER, child, 8*ORIG_RAX, NULL) == SYS_kill){
+					printf("Se ha hecho justicia!\n");
+					kill(child, SIGKILL);
+				}else{
+					ptrace(PTRACE_SYSCALL, child, NULL, NULL);
+				}
+			}else{
+				ptrace(PTRACE_SYSCALL, child, NULL, NULL);
+			}
 		}
 	}
+	ptrace(PTRACE_DETACH, child, NULL, NULL);
 	return 0;
 }
